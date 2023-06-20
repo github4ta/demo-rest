7732ee496a68c0cfcb002ea13bb08c4c096b90fd
Katerina Shidlovskaya <katerinashidlovskaya2711@gmail.com>
2023-06-20 20:11:15 +0300

    add constructor

--- src/main/java/com/typicode/jsonplaceholder/PostObject.java
+++ src/main/java/com/typicode/jsonplaceholder/PostObject.java
@@ -1,8 +1,15 @@
 package com.typicode.jsonplaceholder;
 
 public class PostObject {
-    int userId;
-    int id;
-    String title;
-    String body;
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+
+    public PostObject(int userId, int id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
 }
