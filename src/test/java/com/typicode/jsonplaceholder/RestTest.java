865fc360188397e6c4fc7f736bf47009c867814b
Mikita Shykin <mikita.shikin@gmail.com>
2023-06-20 20:05:07 +0200

    add test and dependency

--- src/test/java/com/typicode/jsonplaceholder/RestTest.java
+++ src/test/java/com/typicode/jsonplaceholder/RestTest.java
@@ -1,4 +1,14 @@
 package com.typicode.jsonplaceholder;
 
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class RestTest {
+
+    @Test
+    public void testGetPostById(){
+        given().when().get("https://jsonplaceholder.typicode.com/posts/1").then().log().body();
+
+    }
 }
