d864d00522652a512887f526941185862ef5882d
Sergei Tsarik <serge.tsarik@gmail.com>
2023-06-22 20:29:37 +0300

    refactor test

--- src/test/java/com/typicode/jsonplaceholder/RestTest.java
+++ src/test/java/com/typicode/jsonplaceholder/RestTest.java
@@ -1,9 +1,11 @@
 package com.typicode.jsonplaceholder;
 
+import io.restassured.response.ValidatableResponse;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class RestTest {
     @Test
@@ -14,9 +16,15 @@ public class RestTest {
     @Test
     public void testCreatePost(){
         PostObject po = new PostObject(1000, 1, "Hello", "Hello from rest");
-        given().header("Content-type","application/json; charset=UTF-8")
-                .body(po).post("https://jsonplaceholder.typicode.com/posts")
-                .then().log().body();
-    }
+        given().header("Content-type","application/json; charset=UTF-8").body(po)
+                .when().post("https://jsonplaceholder.typicode.com/posts").then();
 
+        ValidatableResponse responce = given().header("Content-type","application/json; charset=UTF-8").body(po)
+                .when().post("https://jsonplaceholder.typicode.com/posts").then();
+
+        PostObject poActual = responce.extract().as(PostObject.class);
+
+        Assert.assertEquals(responce.extract().statusCode(), 200);
+        Assert.assertEquals(poActual, po);
+    }
 }
