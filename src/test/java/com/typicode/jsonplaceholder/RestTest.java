78a6379c35b79c4ae199123a534f4b53f450aaa7
Sergei Tsarik <serge.tsarik@gmail.com>
2023-06-24 00:21:41 +0300

    add 21vek

--- src/test/java/com/typicode/jsonplaceholder/RestTest.java
+++ src/test/java/com/typicode/jsonplaceholder/RestTest.java
@@ -84,4 +84,15 @@ public class RestTest {
         formParams.put("USER_REMEMBER", "Y");
         given().when().formParams(formParams).post("https://markformelle.by/ajax/auth_ajax.php").then().log().body();
     }
+
+    @Test
+    public void test21vek() {
+        String body = "{\n" +
+                "    \"User\": {\n" +
+                "        \"email\": \"test@test.com\",\n" +
+                "        \"password\": \"1q2w3e4r\"\n" +
+                "    }\n" +
+                "}";
+        given().when().header("Content-Type", "application/json").body(body).post("https://www.21vek.by/users/login/").then().log().body();
+    }
 }
