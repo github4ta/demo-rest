49bc405c5f5b541807bc3faa789fdf4cc698e0b5
Sergei Tsarik <serge.tsarik@gmail.com>
2023-06-26 07:59:59 +0300

    add vodafone

--- src/test/java/com/typicode/jsonplaceholder/RestTest.java
+++ src/test/java/com/typicode/jsonplaceholder/RestTest.java
@@ -137,4 +137,16 @@ public class RestTest {
 
         given().when().body(body).header("Content-Type", "application/json").post("https://admin.megatop.by/api/v1/user/login").then().log().body();
     }
+
+    @Test
+    public void testVodafone() {
+        String body = "{\n" +
+                "    \"username\": \"\",\n" +
+                "    \"password\": \"1q2w3e4r\",\n" +
+                "    \"returnUrl\": \"/connect/authorize/callback?client_id=ecare-auth-prod1&scope=openid%20vf-profile%20vf-account%20vf-subscription%20vf-contact&response_type=code&redirect_uri=https%3A%2F%2Fmyaccount.vodafone.co.uk%2Fauth%2Flogin%2Fcallback&state=%7B%22successReturnUrl%22%3A%22%252Faccount%252Fsummary%253Ficmp%253Duk~1_consumer~topnav~useractions~1_login_to_my_vodafone%2526linkpos%253Dtopnav~1~1%22,%22errorReturnUrl%22%3A%22https%253A%252F%252Fwww.vodafone.co.uk%252F%22%7D&acr_values=LOA%3A3\",\n" +
+                "    \"userIdentifier\": \"un\"\n" +
+                "}";
+
+        given().when().body(body).header("Content-Type", "application/json").post("https://login.vodafone.co.uk/api/authentication/password").then().log().body();
+    }
 }
